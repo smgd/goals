6b20a3e2101881d86316827cfb4e47d787da6e9d
soapman <soapmangoesdown@gmail.com>
2020-01-06 17:47:04 +0300

    refactor routes

--- app/routes.go
+++ app/routes.go
@@ -1,10 +1,23 @@
 package app
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func (s *server) addRoute(path string, handleFunc func(http.ResponseWriter,
+	*http.Request), methods []string) {
+	methods = append(methods, "OPTIONS")
+	s.router.HandleFunc(fmt.Sprintf("/api/%s", path), s.withCORS(handleFunc)).Methods(methods...)
+}
+
+func (s *server) addPrivateRoute(path string, handleFunc func(http.ResponseWriter,
+	*http.Request), methods []string) {
+	s.addRoute(path, s.privateRoute(handleFunc), methods)
+}
+
 func (s *server) routes() {
-	s.router.HandleFunc("/api/login", s.handleLogin()).Methods("POST")
-	s.router.HandleFunc("/api/register", s.handleRegister()).Methods("POST")
-	s.router.HandleFunc(
-		"/api/whoami",
-		s.withCORS(s.privateRoute(s.handlerWhoAmI())),
-	).Methods("GET", "OPTIONS")
+	s.addRoute("login", s.handleLogin(), []string{"POST"})
+	s.addRoute("register", s.handleRegister(), []string{"POST"})
+	s.addPrivateRoute("whoami", s.handlerWhoAmI(), []string{"GET"})
 }
