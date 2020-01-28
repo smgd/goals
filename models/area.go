bdf78cd919089f632038fadf947c50b236a9ca9e
soapman <soapmangoesdown@gmail.com>
2020-01-28 04:08:14 +0300

    add icon and is_favourite fields to area model

--- models/area.go
+++ models/area.go
@@ -8,6 +8,8 @@ type Area struct {
 	gorm.Model
 	Name        string
 	Description string
+	Icon        string
+	IsFavourite bool `gorm:"default:false"`
 	User        User `gorm:"foreignkey:UserID"`
 	UserID      uint
 }
